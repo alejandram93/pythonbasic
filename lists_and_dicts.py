3bb490ed2515be6ed6bfc385de742ee8c837a578
alejandram93 <alejandra.mora@mottmacdonald.com>
2021-07-30 18:59:50 +1000

    'new data'

--- lists_and_dicts.py
+++ lists_and_dicts.py
@@ -1,24 +1,24 @@
 def run():
     my_list = [1, "Hello", True, 4.5]
-    my_dict = {"firstname": "Facundo", "lastname": "Mora"}
+    my_dict = {"firstname": "Facundo", "lastname": "García"}
 
     super_list = [
-        {"firstname": "Facundo", "lastname": "Mora"},
-        {"firstname": "Isidro", "lastname": "Flores"},
-        {"firstname": "Andres", "lastname": "Garcia"},
-        {"firstname": "Alejandra", "lastname": "Smith"}
-
+        {"firstname": "Facundo", "lastname": "García"},
+        {"firstname": "Miguel", "lastname": "Rodriguez"},
+        {"firstname": "Pablo", "lastname": "Trinidad"},
+        {"firstname": "Susana", "lastname": "Martinez"},
+        {"firstname": "José", "lastname": "Fernandez"},
     ]
 
     super_dict = {
         "natural_nums": [1, 2, 3, 4, 5],
-        "integer_nums": [-1, -2, 0, 1, 2],
-        "floating_nums": [1.1, 2.5, 4.2, 6.5]
+        "integer_nums": [-1, -2, 3, 0, 1],
+        "floating_nums": [1.1, 4.55, 6.43],
     }
 
     for key, value in super_dict.items():
-        print(key, "-", value)
+        print(key, ">", value)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
